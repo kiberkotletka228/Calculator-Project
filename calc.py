f47438920e7bc7b41858d1f18f7fe9d91f997506
kiberkotletka228 <kiberkotletka07@gmail.com>
2026-03-26 19:34:13 +0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,7 +1,7 @@
-# Простой калькулятор - Версия 1
-def subtract(a, b):
-    return a - b
+# Простой калькулятор - Версия 2
+def multiply(a, b):
+    return a * b
 
 num1 = 10
 num2 = 5
-print(f"Результат вычитания {num1} - {num2} = {subtract(num1, num2)}")
+print(f"Результат умножения {num1} * {num2} = {multiply(num1, num2)}")
